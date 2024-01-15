c033bb4b082ce06db57cc47dca7407511847acb4
btaskel <bt_asker@hotmail.com>
2024-01-15 12:06:01 +0800

    timedict 重构.

--- core/modules/timedict/timedict.go
+++ core/modules/timedict/timedict.go
@@ -1,20 +1,24 @@
 package timedict
 
 import (
+	"encoding/binary"
 	"errors"
 	"time"
 )
 
 type TimeDict struct {
-	dict      map[string][]byte // TimeDict存储
+	dict      map[string][][]byte // TimeDict存储
 	closeFlag bool
 }
 
-func (t *TimeDict) set(key string, value byte) {
+func (t *TimeDict) set(key string, value []byte) {
 	if _, ok := t.dict[key]; ok && len(t.dict[key]) < 65535 {
 		t.dict[key] = append(t.dict[key], value)
 	} else {
-		t.dict[key] = []byte{byte(time.Now().Unix())}
+		now := time.Now().Unix()
+		var data []byte
+		binary.BigEndian.PutUint64(data, uint64(now))
+		t.dict[key] = [][]byte{data}
 	}
 	//markLs := t.dict[key]
 
@@ -23,14 +27,14 @@ func (t *TimeDict) set(key string, value byte) {
 func (t *TimeDict) get(key string) (data []byte, err error) {
 	tic1 := time.Now().Unix()
 	for {
-		data := t.dict[key]
-		if data == nil {
-			if time.Now().Unix()-tic1 > 4 {
+		result := t.dict[key]
+		if result == nil {
+			if tic1-int64(binary.BigEndian.Uint64(result[0])) > 4 {
 				err = errors.New("timeout")
 				break
 			}
 		} else {
-			return
+			return result[1], err
 		}
 	}
 	return
@@ -53,5 +57,18 @@ func (t *TimeDict) closeTimeDict() {
 }
 
 func (t *TimeDict) release() {
+	for {
+		if !t.closeFlag {
+			t.dict = make(map[string][][]byte)
+			return
+		}
 
+		for key, value := range t.dict {
+			timeStamp := value[0]
+			if time.Now().Unix()-int64(binary.BigEndian.Uint64(timeStamp)) > 8 {
+				delete(t.dict, key)
+			}
+		}
+
+	}
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module EXSync
+
+go 1.21.3
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
